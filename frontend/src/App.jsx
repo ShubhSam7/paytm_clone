b9fcb4c6c819833eb08dbb89caa54e0a1a12955a
ShubhSam7 <shubhsamaiya7@gmail.com>
2025-12-16 14:43:46 +0530

    added signup/signin button with Shadcn

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,42 @@
+import { Button } from "@/components/ui/button";
+import { useNavigate, Routes, Route } from "react-router";
+import Signup from "./SignUp";
+import Signin from "./SignIn";
 
-function App() {
+function Home() {
+  const navigate = useNavigate();
 
   return (
-    <div>
-        Hello world
+    <div className="flex min-h-svh flex-col items-center justify-center gap-4">
+      <h1 className="text-2xl font-bold">Welcome to Paytm Clone</h1>
+      <div className="flex gap-4">
+        <Button
+          onClick={() => {
+            navigate("/signup");
+          }}
+        >
+          SignUp
+        </Button>
+        <Button
+          onClick={() => {
+            navigate("/signin");
+          }}
+        >
+          SignIn
+        </Button>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/signup" element={<Signup />} />
+      <Route path="/signin" element={<Signin />} />
+    </Routes>
+  );
+}
+
+export default App;
---
+++ frontend/src/SignIn.jsx
@@ -0,0 +1,7 @@
+const Signin = () => {
+    return<div>
+        But now this is the Signin page
+    </div>
+}
+
+export default Signin;
